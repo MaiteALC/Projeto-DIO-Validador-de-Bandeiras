1a11becd52411d6f99cb204cd83361df300f4121
MaiteALC <maitexp549@gmail.com>
2025-05-27 17:49:07 -0300

    Atualizar o validador.py

--- validador.py
+++ validador.py
@@ -4,14 +4,41 @@ mastercard = ("51", "52", "53", "54", "55", "2221", "2222", "2223", "2224", "222
 
 # Lista de bandeiras aceitas para validação.
 lista_bandeiras = ["Visa", "MasterCard", "Elo", "AmericanExpress", "Discover", "HiperCard", "JCB", "Aura", "Não Identificada"]
+  
+def  converter_digito(digito):
+    return int(digito)
 
-numero_cartao = input("Insira o número do seu cartão: ")
+numero = input("Insira o número do seu cartão: ")
 
-qntd_digitos = len(numero_cartao)
+while len(numero) < 14:
+    numero = input("O número deve ter 14 dígitos, tente novamente: ") 
 
-if qntd_digitos < 14:
-  print("Número do cartão inválido")
+algoritmo_luhn = filter(str.isdigit, numero)
 
+lista_digitos = list(map(converter_digito, algoritmo_luhn))
+
+print(f"Lista dos digitos: {lista_digitos}")
+
+def validador_luhn(lista_num_cartao):
+    indices_impares = []
+    indices_pares = []
+    for indice, elemento in enumerate(lista_num_cartao[::-1]):
+        if indice % 2 != 0:
+            dobrado = elemento * 2
+            if dobrado > 9:
+                    indices_pares.append(dobrado - 9)
+            else:
+                    indices_pares.append(dobrado)
+        elif indice % 2 == 0:
+            indices_impares.append(elemento)
+    soma = sum(indices_pares) + sum(indices_impares)
+    return soma 
+  
+luhn = validador_luhn(lista_digitos)
+           
+while not luhn % 10 == 0:
+    print("Número do cartão inválido.")
+    numero = input("Digite novamente: ")
 
 # Função que recebe o número do cartão e retorna a bandeira correspondente.
 def validador_bandeira(cartao):
@@ -31,12 +58,10 @@ def validador_bandeira(cartao):
     bandeira = lista_bandeiras[6]
   elif cartao.startswith("50"):
     bandeira = lista_bandeiras[7]
-  else: 
-    bandeira = lista_bandeiras[-1]
+  else:
+      bandeira = lista_bandeiras[-1]
   return bandeira
 
-validacao = validador_bandeira(numero_cartao)
+bandeira_validada = validador_bandeira(numero)
 
-print(f"Bandeira do cartão: " + str(validacao))
-  
-      
+print(f"Bandeira identificada: {bandeira_validada}")
